69867ad4ac406830a952608fb90663074facbef5
Raz <razaljaf@gmail.com>
2022-11-14 08:54:14 -0500

    styling and functionality in order for hero page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import HeroSection from './components/heroSection/HeroSection'
 function App() {
   return (
-    <div className="App"> We are live</div>
+    <Router>
+      <div className="App">
+      </div>
+      <Routes>
+        <Route exact path='/' element={<HeroSection />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from "styled-components";
+import Logo from '../../assets/Logo White.png';
+import DronePicture from '../../assets/Drone.png';
+import { Link } from 'react-router-dom';
+
+function HeroSection (){
+
+    const StyledHeroSection = styled.div`
+        background-color: orange;
+        height: 100%;
+        width: 100%;
+    `;
+    const StyledLogo = styled.img`
+        height: 50px;
+        width: 150px;
+        padding: 24px;
+    `;
+    const StyledHeader = styled.div`
+        display: flex;
+        justify-content: center;
+    `;
+    const StyledHeroPicture = styled.img`
+        height: 400px;
+        width: 500px;
+        padding: 50px;
+    `;
+    const StyledBody = styled.div`
+        display: flex;
+        justify-content: space-around;
+    `;
+    const SideBarLinks = styled.div`
+        display: block;
+        padding: 16px;
+    `;
+    const ListedLink = styled.li`
+        font-family: inherit;
+        font-size: x-large;
+        color: white;
+    `;
+
+    return (
+        <StyledHeroSection>
+            <StyledHeader>
+                <StyledLogo src={Logo}/>
+            </StyledHeader>
+            <StyledBody>    
+                <StyledHeroPicture src={DronePicture} />
+                <SideBarLinks>
+                    <ListedLink>
+                        <Link to='/home'>Home</Link>
+                    </ListedLink>
+                    <ListedLink>
+                        <Link to='/contact'>Contact</Link>
+                    </ListedLink>
+                </SideBarLinks>
+            </StyledBody>
+        </StyledHeroSection>
+    );
+};
+
+export default HeroSection;
